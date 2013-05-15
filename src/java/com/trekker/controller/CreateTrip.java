da136ce6f54d71ea5d77cd0ed1ed58a449751146
Joseph Skufca <jdskufca@gmail.com>
2013-05-15 08:09:49 -0400

    Convert create trip to date picker

--- src/java/com/trekker/controller/CreateTrip.java
+++ src/java/com/trekker/controller/CreateTrip.java
@@ -20,17 +20,6 @@ public class CreateTrip {
     private Trip trip;
     private User user;
     
-    private int startYear;
-    private int startMonth;
-    private int startDay;
-    
-    private int endYear;
-    private int endMonth;
-    private int endDay;
-    
-    @EJB
-    private TripService tripService;
-    
     @EJB
     private UserService userService;
     
@@ -38,16 +27,9 @@ public class CreateTrip {
     public void init() {
         trip = new Trip();
         user = userService.currentUser();
-        
-        startMonth = -1;
-        endMonth = -1;
     }
     
     public void submit() throws IOException {
-        Date startDate = new Date(startYear - 1900, startMonth, startDay);
-        Date endDate = new Date(endYear - 1900, endMonth, endDay);
-        trip.setStartDate(startDate);
-        trip.setEndDate(endDate);
         trip.setOwner(user);
         Collection<Trip> trips = user.getTrips();
         trips.add(trip);
@@ -60,54 +42,4 @@ public class CreateTrip {
     public Trip getTrip() {
         return trip;
     }
-
-    public int getStartYear() {
-        return startYear;
-    }
-
-    public void setStartYear(int startYear) {
-        this.startYear = startYear;
-    }
-
-    public int getStartMonth() {
-        return startMonth;
-    }
-
-    public void setStartMonth(int startMonth) {
-        this.startMonth = startMonth;
-    }
-
-    public int getStartDay() {
-        return startDay;
-    }
-
-    public void setStartDay(int startDay) {
-        this.startDay = startDay;
-    }
-
-    public int getEndYear() {
-        return endYear;
-    }
-
-    public void setEndYear(int endYear) {
-        this.endYear = endYear;
-    }
-
-    public int getEndMonth() {
-        return endMonth;
-    }
-
-    public void setEndMonth(int endMonth) {
-        this.endMonth = endMonth;
-    }
-
-    public int getEndDay() {
-        return endDay;
-    }
-
-    public void setEndDay(int endDay) {
-        this.endDay = endDay;
-    }
-    
-    
 }
